b7893a4501eb385aba062f688095098304ad8777
Per Lundberg <perlun@gmail.com>
2020-06-21 23:16:44 +0300

    Initial spike with .NET Core sitegen implementation
    - `make autobuild` works, rebuilding things based on FS events.
    
    - `make serve` works, and even supports live-reload. This is great, even
      better than what I had with Jekyll!
    
    (Note: the above depends on a `Makefile` not currently present in this
    repo. I hope to be able to provide an example in this repo in the
    future.)
    
    What remains:
    
    - Some things are hardwired in Program.cs; this is a bit ugly. We should
      rightfully move them to a config.toml. I've researched this part and
      the `nett` library seems to be a reasonable choice here.
    
    - The whole blog post logic is missing. We should parse the files in
      _post and make a list of blog posts be available somewhere in the global
      context. This is required to make the front page work.
    
    - We should render the blog posts to HTML, using `markdig`. It's
      obviously a good choice since both I and StackOverflow are in
      agreement here. ;)
    
    - We should make category pages be generated whenever a blog post is
      edited. This is required to get us on-par with what we currently have
      in Jekyll
    
    Once we have reached this stage, we should be pretty much 1-to-1
    feature-wise with the current site. We can then start building some of
    the new functionality:
    
    - Support for /sv/om_oss and /about being logically connected (one being
      the Swedish About page, the other being the "default" English about
      page). Can perhaps do this as a static list in config.toml or
      something, just the easiest possible approach is fine for our
      particular use case.
    
    - Adding more content. Thinking about it more, the approach above (with
      a manual wiring in config.toml) will not really scale once we start
      digging into the bible study project which I am envisioning. I want
      things like /bible_study/genesis/1 and /sv/bibelstudier/1_mosebok/1 to
      be logically connected, and I definitely do not want to do this for
      every single chapter in the bible; that will drive me insane. Perhaps
      there is a way to define "patterns" or something? I can live with
      defining 66 patterns (one for each book in the Bible) in the config, I
      guess.

---
+++ SiteGenerator.ConsoleApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using HandlebarsDotNet;
+
+namespace SiteGenerator.ConsoleApp
+{
+    public static class Program
+    {
+        // TODO: Make configurable. Ideally, support something like gcc's -I parameter which can be provided multiple
+        // TODO: times. Perhaps the easiest way is to just make this a setting in config.toml? Yes, that'll
+        // TODO: probably be the case.
+        private const string SourceDir = "src";
+
+        public static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Syntax: sitegen <src-file> <target-file>");
+                Environment.Exit(1);
+            }
+
+            string source = File.ReadAllText(args[0]);
+
+            Handlebars.RegisterHelper("include", IncludeHelper);
+
+            var template = Handlebars.Compile(source);
+
+            var data = new {
+                title = "My new post",
+                body = "This is my first post!",
+                now = DateTime.Now,
+                site = new {
+                    Title = "halleluja.nu"
+                }
+            };
+
+            var result = template(data);
+
+            File.WriteAllText(args[1], result);
+        }
+
+        private static void IncludeHelper(TextWriter writer, dynamic context, object[] parameters)
+        {
+            if (parameters.Length != 1)
+            {
+                throw new HandlebarsException("{{include}} helper must have exactly one argument");
+            }
+
+            if (!(parameters[0] is string fileName))
+            {
+                throw new HandlebarsException("{{include}} expected string parameter, not " + parameters[0].GetType().Name);
+            }
+
+            string templateSource = File.ReadAllText(Path.Join(SourceDir, fileName));
+
+            var template = Handlebars.Compile(templateSource);
+            string result = template(context);
+
+            writer.WriteSafeString(result);
+        }
+    }
+}
