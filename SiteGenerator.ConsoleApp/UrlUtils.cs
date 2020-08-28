27c204fbace1495e28ba636924e4ea4f4475c16a
Per Lundberg <perlun@gmail.com>
2020-08-28 23:29:37 +0300

    (UrlUtils) Bug fixes for slugification (#6)
    - Remove existing dashes
    - Replace multiple spaces with a single space
    - Other minor improvements

--- SiteGenerator.ConsoleApp/UrlUtils.cs
+++ SiteGenerator.ConsoleApp/UrlUtils.cs
@@ -1,7 +1,11 @@
 namespace SiteGenerator.ConsoleApp
 {
-    public class UrlUtils
+    public static class UrlUtils
     {
+        // Note: the algorithm below is obviously utterly inefficient in that it performs n passes to perform the
+        // slugification, when a single pass doing it all-at-once would be tons more efficient, both in terms of
+        // runtime speed and GC pressure. This is only suited for running on short strings, where those factors are
+        // practically irrelevant.
         public static string Slugify(string s) => s
             .ToLower()
 
@@ -11,8 +15,19 @@ namespace SiteGenerator.ConsoleApp
             .Replace('ä', 'a')
             .Replace('ö', 'o')
 
-            // Dashes work better than spaces in URLs.
+            // Existing dashes can be removed...
+            .Replace("-", "")
+            .Replace("&mdash;", "")
+
+            // ...but then again, spaces are converted to dashes, since they work better in URLs.
             .Replace(' ', '-')
+
+            // Multiple spaces (dashes) get converted to single space. This would obviously have to run multiple times
+            // to handle scenarios with _more_ than two spaces, but for our very simplistic existing use cases, this
+            // is again sufficient.
+            .Replace("--", "-")
+
+            .Replace(",", "")
 
             // We consider certain characters "forbidden" or unsuitable from being used in URLs; we simply strip them
             // out when generating the slugs.
